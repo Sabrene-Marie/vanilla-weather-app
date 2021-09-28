f725f19ac82876c10217a54815c65be1348f1fb6
Sabrene-Marie <91009366+Sabrene-Marie@users.noreply.github.com>
2021-09-28 20:33:13 +1000

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -43,5 +43,19 @@ let apiKey = "a20ef2f2becea8997e30e5c36e64613d"
 let city = "Sydney"; 
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
 
+function search(city) {
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
 
 axios.get(apiUrl).then(displayTemperature); 
