114ffa38826b38b30145ec8461ace564b72c3820
Sabrene-Marie <91009366+Sabrene-Marie@users.noreply.github.com>
2021-09-28 18:53:32 +1000

    Showing the current temperature, wind, humidity and weather description through javascript.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,15 @@
 function displayTemperature (response){
     let temperatureElement = document.querySelector("#temperature"); 
+    let cityElement = document.querySelector("#city"); 
+    let descriptionElement = document.querySelector("#description"); 
+    let humidityElement = document.querySelector("#humidity"); 
+    let windElement = document.querySelector("#wind"); 
     temperatureElement.innerHTML = Math.round
     (response.data.main.temp); 
+    cityElement.innerHTML=response.data.name;
+    descriptionElement.innerHTML=response.data.weather[0].description; 
+    humidityElement.innerHTML=response.data.main.humidity; 
+    windElement.innerHTML=Math.round(response.data.wind.speed);
 }
 let apiKey = "a20ef2f2becea8997e30e5c36e64613d"
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=sydney&appid=${apiKey}&units=metric`
