6de88f37efab8c93fc1c63f4a499058d127d85b5
Sabrene-Marie <91009366+Sabrene-Marie@users.noreply.github.com>
2021-09-28 18:18:59 +1000

    Added temperature Javascript and edited fonts in CSS.

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
+function displayTemperature (response){
+    let temperatureElement = document.querySelector("#temperature"); 
+    temperatureElement.innerHTML = Math.round
+    (response.data.main.temp); 
+}
+let apiKey = "a20ef2f2becea8997e30e5c36e64613d"
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=sydney&appid=${apiKey}&units=metric`
+console.log(apiUrl)
 
+axios.get(apiUrl).then(displayTemperature); 
