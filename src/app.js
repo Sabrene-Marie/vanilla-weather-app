0a5a98a31bbb8e8ef3857b255703e45a2c5e6927
Sabrene-Marie <91009366+Sabrene-Marie@users.noreply.github.com>
2021-09-30 19:10:56 +1000

    added current location button and CSS. Edited CSS as well.

--- src/app.js
+++ src/app.js
@@ -1,47 +1,58 @@
-function formatDate (timestamp){
-let date = new Date (timestamp);
-let hours = date.getHours();
-if (hours < 10){
-    hours = `0${hours}`; 
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
 }
-let minutes = date.getMinutes(); 
-if (minutes < 10){
-    minutes = `0${minutes}`; 
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
 }
-let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ];
-let day = days [date.getDay()]; 
-return `${day} ${hours}:${minutes}`; 
-}
 
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
 
+  celsiusTemperature = response.data.main.temp;
 
-
-
-function displayTemperature (response){
-    let temperatureElement = document.querySelector("#temperature"); 
-    let cityElement = document.querySelector("#city"); 
-    let descriptionElement = document.querySelector("#description"); 
-    let humidityElement = document.querySelector("#humidity"); 
-    let windElement = document.querySelector("#wind"); 
-    let dateElement = document.querySelector("#date"); 
-    let iconElement= document.querySelector("#icon");
-    temperatureElement.innerHTML = Math.round
-    (response.data.main.temp); 
-    cityElement.innerHTML=response.data.name;
-    descriptionElement.innerHTML=response.data.weather[0].description; 
-    humidityElement.innerHTML=response.data.main.humidity; 
-    windElement.innerHTML=Math.round(response.data.wind.speed);
-    dateElement.innerHTML= formatDate(response.data.dt* 1000); 
-    iconElement.setAttribute(
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
-
 }
-let apiKey = "a20ef2f2becea8997e30e5c36e64613d"
-let city = "Sydney"; 
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
 
 function search(city) {
   let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
@@ -55,7 +66,28 @@ function handleSubmit(event) {
   search(cityInputElement.value);
 }
 
+function searchLocation(position) {
+  let apiKey = "abf76d7ef1db28a34af0244160e372f0";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+function displayWeatherCondition(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+}
+
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
-axios.get(apiUrl).then(displayTemperature); 
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
+search("Sydney");
